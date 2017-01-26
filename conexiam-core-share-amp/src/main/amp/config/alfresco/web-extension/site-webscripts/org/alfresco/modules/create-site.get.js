ee8c01b82e5124dbefb968252a6c5a17d645fc46
jpotts <jeffpotts01@gmail.com>
2017-01-25 23:14:36 -0600

    Initial population

---
+++ conexiam-core-share-amp/src/main/amp/config/alfresco/web-extension/site-webscripts/org/alfresco/modules/create-site.get.js
@@ -0,0 +1,7 @@
+var sitePresets = [
+   {id: "site-dashboard", name: msg.get("title.collaborationSite")},
+   {id: "paid-engagement", name: msg.get("title.paidEngagement")}
+];
+model.sitePresets = sitePresets;
+
+model.defaultVisibility = "PRIVATE";
